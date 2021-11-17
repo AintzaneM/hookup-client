f4ccd7fa5f95e83bc1296783a121eb34d25374ca
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-11-17 16:46:14 +0100

    homepage text

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,7 +7,10 @@ class Homepage extends React.Component {
     render(){
         return (            
         <div className="Homepage">
-            <p>Welcome to the homepage</p>
+            <p>HookUp takes the job search to a more intimate level by <strong>sharing professional experiences </strong>
+                in different areas and matching them with <strong>people looking for a new job or deciding which studies to choose.</strong> 
+                <br/>
+                You build culture by talking to people, and understanding what they care about so let's use <strong>HookUp</strong> to start a new professional path.</p>
         </div>
         )
     }
