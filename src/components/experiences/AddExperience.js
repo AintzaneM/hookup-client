666000e692b04c8ba80c5202e1c1d20a058c50b2
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-11-18 08:58:40 +0100

    dummy commit

--- src/components/experiences/AddExperience.js
+++ src/components/experiences/AddExperience.js
@@ -14,6 +14,7 @@ class AddExperience extends Component {
         }
     }
 
+    
     toggleForm =() =>{
         if(!this.state.clickButtonForm) {
             this.setState({clickButtonForm : true});
