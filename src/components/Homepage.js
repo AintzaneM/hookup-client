5fecf2921043922b95c481dc0e5d0ee2e6ac5930
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-11-19 09:07:36 +0100

    change name homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,10 +8,10 @@ class Homepage extends React.Component {
         return (
             <>
                 <p className="homepage-question"><strong>Are you ready to start your own career path?</strong></p>
-                <p className=""> You build culture by talking to people and understanding what they care about so let's use <strong>HookUp</strong> to learn about different professional experiences, according to the most demanded skills in the labor market</p>
+                <p className=""> You build culture by talking to people and understanding what they care about so let's use <strong>About Skills</strong> to learn about different professional experiences, according to the most demanded skills in the labor market</p>
                 <div className="Homepage">
                     <img src="./Diseño sin título.jpg" alt="" />
-                    <p className="homepage-text"><strong>HookUp</strong> takes the job search to a more intimate level by <strong>sharing professional experiences </strong>
+                    <p className="homepage-text"><strong>About Skills</strong> takes the job search to a more intimate level by <strong>sharing professional experiences </strong>
                         in different areas and matching them with <strong>people looking for a new job or deciding which studies to choose.</strong>
                     </p>
                 </div>
